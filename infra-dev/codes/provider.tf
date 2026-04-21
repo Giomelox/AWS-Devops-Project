bce734c091096f92bf61480732dce4ef1d869476
Giovanni Abud <emaildegio@gmail.com>
2026-04-20 22:05:53 -0300

    Adição de Arquivo YAML para automação do GitHub Actions

--- infra-dev/codes/provider.tf
+++ infra-dev/codes/provider.tf
@@ -1,5 +1,14 @@
 terraform {
+
+  backend "s3" {
+    bucket = "projeto-docker-terraform-state"
+    key    = "infra-dev/terraform.tfstate"
+    region = "us-east-2"
+    encrypt = true
+  }
+
   required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
