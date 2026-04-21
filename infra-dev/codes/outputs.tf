13ed94d2c0b7e43354add73ea74abe8323f7ce97
Giovanni Abud <emaildegio@gmail.com>
2026-04-21 17:02:33 -0300

    Alteração do formato de output do terraform

--- infra-dev/codes/outputs.tf
+++ infra-dev/codes/outputs.tf
@@ -1,11 +1,11 @@
 output "ip_publico_ec2" {
-  value = aws_instance.site_estatico_ec2.public_ip
+  value = "IP público da EC2: ${aws_instance.site_estatico_ec2.public_ip}"
 }
 
 output "id_ec2" {
-  value = aws_instance.site_estatico_ec2.id
+  value = "EC2 criada com ID: ${aws_instance.site_estatico_ec2.id}"
 }
 
 output "aws_ecr_repository_url" {
-  value = aws_ecr_repository.site_estatico.repository_url
+  value = "URL do repositório ECR: ${aws_ecr_repository.site_estatico.repository_url}"
 }
