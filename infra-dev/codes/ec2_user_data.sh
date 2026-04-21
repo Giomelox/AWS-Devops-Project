143387cf527f6efdfe58e18035570ee1d952397b
Giovanni Abud <emaildegio@gmail.com>
2026-04-21 16:59:35 -0300

    Adição do SSM Agent no user_data e alteração nas versões do hashcorp

--- infra-dev/codes/ec2_user_data.sh
+++ infra-dev/codes/ec2_user_data.sh
@@ -12,3 +12,10 @@ systemctl enable docker
 
 # libera uso do docker sem sudo
 usermod -aG docker ec2-user
+
+# =========================
+# SSM AGENT (ESSENCIAL PARA QUE EU CONSIGA ME CONECTAR NA INSTÂNCIA PELO AWS SYSTEM MANAGER)
+# =========================
+yum install -y amazon-ssm-agent
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
--- infra-dev/codes/outputs.tf
+++ infra-dev/codes/outputs.tf
@@ -2,6 +2,10 @@ output "ip_publico_ec2" {
   value = aws_instance.site_estatico_ec2.public_ip
 }
 
+output "id_ec2" {
+  value = aws_instance.site_estatico_ec2.id
+}
+
 output "aws_ecr_repository_url" {
   value = aws_ecr_repository.site_estatico.repository_url
 }
